4bdfecc7a04500ce0f0c98228b9cd56c01f16bdd
dxcswl <dxcswl@163.com>
2017-11-13 14:38:08 +0800

    *

--- src/org/Weixin/WxPay.Config.php
+++ src/org/Weixin/WxPay.Config.php
@@ -23,10 +23,10 @@ class WxPayConfig
 	 * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
 	 * @var string
 	 */
-	const APPID = 'wx426b3015555a46be';
-	const MCHID = '1900009851';
-	const KEY = '8934e7d15453e97507ef794cf7b0519d';
-	const APPSECRET = '7813490da6f1265e4901ffb80afaa36f';
+	const APPID = '';
+	const MCHID = '';
+	const KEY = '';
+	const APPSECRET = '';
 	
 	//=======【证书路径设置】=====================================
 	/**
