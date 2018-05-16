badacb948d28a321862178c52194bdc15219b51f
dxcswl <dxcswl@163.com>
2018-05-16 10:13:36 +0800

    *

--- src/Weixin.php
+++ src/Weixin.php
@@ -37,7 +37,7 @@ class Weixin {
         }else{
             $result = $result_code['data'];
         }
-        if(isset($result['appid']) && isset($result['nonce_str']) && isset($result['mch_id']) && isset($result['prepay_id']) && isset($result['sign'])) {
+        if(isset($result['appid']) and isset($result['nonce_str']) and isset($result['mch_id']) and isset($result['prepay_id']) and isset($result['sign'])) {
             $data['appid'] = $result['appid'];
             $data['partnerid'] = $result['mch_id'];
             $data['prepayid'] = $result['prepay_id'];
@@ -45,6 +45,32 @@ class Weixin {
             $data['noncestr'] = $result['nonce_str'];
             $data['timestamp'] = time();
             $data['sign'] = self::MakeSign($data, $config['key']);
+            $data['sign_original'] = $result['sign'];
+            return ['code' => 1, 'msg' => 'OK', 'data' => $data];
+        } else {
+            if(isset($result['return_msg'])) {
+                $result['msg'] = $result['return_msg'];
+            }
+            return ['code' => 99, 'msg' => $result['msg'], 'data' => []];
+        }
+    }
+
+    public static function get_weixinWeb($config, $total_price, $order_id, $pay_id){
+
+        $result_code = self::getWeixinUnifiedOrder($config, $total_price, $order_id, $pay_id,'JSAPI');
+        if($result_code['code'] == 0){
+            return $result_code;
+        }else{
+            $result = $result_code['data'];
+        }
+        if(isset($result['appid']) and isset($result['nonce_str']) and isset($result['mch_id']) and isset($result['prepay_id']) and isset($result['sign'])) {
+            $data['appid'] = $result['appid'];
+            $data['partnerid'] = $result['mch_id'];
+            $data['prepayid'] = $result['prepay_id'];
+            $data['package'] = 'prepay_id='.$result['prepay_id'];
+            $data['noncestr'] = $result['nonce_str'];
+            $data['timestamp'] = time();
+            $data['sign'] = self::MakeSign(['appId'=>$data['appid'],'timeStamp'=>''.$data['timestamp'],'nonceStr'=>$data['noncestr'],'package'=>$data['package'],'signType'=>'MD5'], $config['key']);
             $data['sign_original'] = $result['sign'];
             return ['code' => 1, 'msg' => 'OK', 'data' => $data];
         } else {
@@ -81,8 +107,18 @@ class Weixin {
                 'notify_url' => $config['notify_url'],
                 'trade_type' => $trade_type,
             ];
+            if($trade_type == "JSAPI"){
+                $data['openid'] = $config['openid'];
+            }elseif($trade_type == "NATIVE"){
+                $data['product_id'] = $config['product_id'];
+            }
+
+
             $data['sign'] = self::MakeSign($data, $config['key']);
             $xml = self::ToXml($data);
+            /*if($trade_type == 'JSAPI'){
+                var_dump($xml);exit();
+            }*/
             if($xml == false){
                 return ['code' => 0, 'msg' => '统一支付(发送)xml数据异常', 'data' => []];
             }else{
@@ -414,6 +450,151 @@ class Weixin {
             }else{
                 return $check_sign;
             }
+        }
+    }
+
+    /** $data 格式如下
+     *  $data = array(
+     * 'openid' //退款者openid
+     * 'refundid' //退款申请ID
+     * 'money' //退款金额
+     * 'desc'  //退款描述
+     * );
+     *
+     */
+    public static function set_refund($data, $wxchat = [])
+    {
+        //判断有没有CA证书及支付信息
+        if (empty($wxchat['api_cert']) || empty($wxchat['api_key']) || empty($wxchat['api_ca']) || empty($wxchat['appid']) || empty($wxchat['mchid'])) {
+            return false;
+//            $wxchat['appid'] = $this->appid;
+//            $wxchat['mchid'] = $this->mchid;
+//            $wxchat['api_cert'] = APP_PATH .$this->cacab['api_cert'];
+//            $wxchat['api_key'] = APP_PATH .$this->cacab['api_key'];
+//            $wxchat['api_ca'] = APP_PATH .$this->cacab['api_ca'];
+        }else{
+
+        }
+        $webdata = array(
+            'mch_appid' => $wxchat['appid'],
+            'mchid' => $wxchat['mchid'],
+            'nonce_str' => md5(time()),
+            //'device_info' => '1000',
+            'partner_trade_no' => $data['refundid'], //商户订单号，需要唯一
+            'openid' => $data['openid'],
+            'check_name' => 'NO_CHECK', //OPTION_CHECK不强制校验真实姓名, FORCE_CHECK：强制 NO_CHECK：
+            //'re_user_name' => 'jorsh', //收款人用户姓名
+            'amount' => $data['money'] * 100, //付款金额单位为分
+            'desc' => empty($data['desc']) ? '提现' : $data['desc'],
+            'spbill_create_ip' => self::refund_getip(),
+        );
+        foreach ($webdata as $k => $v) {
+            $tarr[] = $k . '=' . $v;
+        }
+        sort($tarr);
+        $sign = implode($tarr, '&');
+        $sign .= '&key=' . $wxchat['signkey'];
+        $webdata['sign'] = strtoupper(md5($sign));
+        $wget = self::refund_array2xml($webdata);
+        $res = self::refund_http_post('https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers', $wget, $wxchat);
+        if (!$res) {
+            return array('status' => 0, 'msg' => "Can't connect the server");
+        }
+        $content = simplexml_load_string($res, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if (strval($content->return_code) == 'FAIL') {
+            return array('status' => 0, 'msg' => strval($content->return_msg));
+        }
+        if (strval($content->result_code) == 'FAIL') {
+            return array('status' => 0, 'msg' => strval($content->err_code), ':' . strval($content->err_code_des));
+        }
+        $rdata = array(
+            'mch_appid' => strval($content->mch_appid),
+            'mchid' => strval($content->mchid),
+            'device_info' => strval($content->device_info),
+            'nonce_str' => strval($content->nonce_str),
+            'result_code' => strval($content->result_code),
+            'partner_trade_no' => strval($content->partner_trade_no),
+            'payment_no' => strval($content->payment_no),
+            'payment_time' => strval($content->payment_time),
+        );
+        return array('status' => 1, 'msg' => '成功打款','data'=>$rdata);
+    }
+
+    public static function refund_getip()
+    {
+        static $ip = '';
+        $ip = $_SERVER['REMOTE_ADDR'];
+        if (isset($_SERVER['HTTP_CDN_SRC_IP'])) {
+            $ip = $_SERVER['HTTP_CDN_SRC_IP'];
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']) AND preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
+            foreach ($matches[0] AS $xip) {
+                if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+                    $ip = $xip;
+                    break;
+                }
+            }
+        }
+        return $ip;
+    }
+
+    /**
+     * 将一个数组转换为 XML 结构的字符串
+     * @param array $arr 要转换的数组
+     * @param int $level 节点层级, 1 为 Root.
+     * @return string XML 结构的字符串
+     */
+    public static function refund_array2xml($arr, $level = 1)
+    {
+        $s = $level == 1 ? "<xml>" : '';
+        foreach ($arr as $tagname => $value) {
+            if (is_numeric($tagname)) {
+                $tagname = $value['TagName'];
+                unset($value['TagName']);
+            }
+            if (!is_array($value)) {
+                $s .= "<{$tagname}>" . (!is_numeric($value) ? '<![CDATA[' : '') . $value . (!is_numeric($value) ? ']]>' : '') . "</{$tagname}>";
+            } else {
+                $s .= "<{$tagname}>" . self::refund_array2xml($value, $level + 1) . "</{$tagname}>";
+            }
+        }
+        $s = preg_replace("/([\x01-\x08\x0b-\x0c\x0e-\x1f])+/", ' ', $s);
+        return $level == 1 ? $s . "</xml>" : $s;
+    }
+
+    public static function refund_http_post($url, $param, $wxchat)
+    {
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        }
+        if (is_string($param)) {
+            $strPOST = $param;
+        } else {
+            $aPOST = array();
+            foreach ($param as $key => $val) {
+                $aPOST[] = $key . "=" . urlencode($val);
+            }
+            $strPOST = join("&", $aPOST);
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($oCurl, CURLOPT_POST, true);
+        curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+        if ($wxchat) {
+            curl_setopt($oCurl, CURLOPT_SSLCERT,   $wxchat['api_cert']);
+            curl_setopt($oCurl, CURLOPT_SSLKEY,  $wxchat['api_key']);
+            curl_setopt($oCurl, CURLOPT_CAINFO,  $wxchat['api_ca']);
+        }
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
         }
     }
 }
